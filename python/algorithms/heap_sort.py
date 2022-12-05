418a2f40191adc44f4b1a0a2e260066f1beb8fd8
Alex Mitra <alexmitra@chromium.org>
2022-12-05 19:36:58 +0000

    Shortens the code for heap sort to two lines.

--- python/algorithms/heap_sort.py
+++ python/algorithms/heap_sort.py
@@ -1,12 +1,11 @@
 import heapq
 import random
 
+
 def heap_sort(iterable):
-    sorted_list = []
     heapq.heapify(iterable)
-    while iterable:
-        sorted_list.append(heapq.heappop(iterable))
-    return sorted_list
+    return [heapq.heappop(iterable) for i in range(len(iterable))]
+
 
 if __name__ == "__main__":
     numbers = [18, 9, 15, 4, 0, 13, 11, 8, 3, 10, 1, 14, 6, 7, 5, 16, 2, 17, 12]
